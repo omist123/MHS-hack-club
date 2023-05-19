bc54c85bd21760f7859e9b5c94dab9abc2bf87ff
omist123 <owenmistretta@gmail.com>
2023-05-19 13:37:44 -0400

    image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,14 @@
-function Home() {
-    return (
-        <div className="flex flex-col justify-center items-center text-3xl">
-            <h1 className="text-red-500 pt-10">hello! </h1>
-            <h1 className="text-red-500 pt-10">hello! </h1>
-            <h1 className="text-red-500 pt-10">hello! </h1>
-            <h1 className="text-red-500 pt-10">hello! </h1>
-            <h1 className="text-red-500 pt-10">hello! </h1>
+import { useState } from "react";
 
+function Home() {
+
+    return (
+        <div className="flex flex-col spcae-between justify-between items-center text-3xl">
+            <h1 className="text-red-500 pt-10">Melrose High School Hack Club</h1> 
+            
+            <img className="pt-10 w-52 h-54" src="https://jgpr.net/wp-content/uploads/2019/12/melrose-logo-1-300x300.png" alt="" /> 
         </div>
+
     );  
 }
 export default Home;    
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,6 +18,7 @@ function Navbar() {
           <a className="contact-btn" href="/#">
             Contact
           </a>
+
           <button className="nav-btn nav-close-btn" onClick={showNavbar}>
             <FaTimes />
           </button>
