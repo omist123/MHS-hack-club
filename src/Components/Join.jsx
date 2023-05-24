1b58c9ab58b9a5e974ddf2131413dd02960ce166
omist123 <owenmistretta@gmail.com>
2023-05-24 11:08:42 -0400

    embed's

--- src/Components/Join.jsx
+++ src/Components/Join.jsx
@@ -2,8 +2,8 @@ function Join() {
   return (
     <>
       <div className="flex flex-col items-center justify-center overflow-auto">
-        <h1 className="text-red-500 pt-20 text-3xl">Fill out this form to join!</h1>
-        <iframe className="pt-10 rounded-md" src="https://docs.google.com/forms/d/e/1FAIpQLScUtkN0THdFEJvHTnnC7pLSlpH7rUDlM8ojdDg5uyrREIk35Q/viewform?embedded=true" width="640" height="1248" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <h1 className="text-red-500 pt-10 text-3xl">Fill out this form to join!</h1>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe95N_MxtiiWM3XZB49AK-lUFF0Y6b0I-0jZr0JJScSPsBtmw/viewform?embedded=true" width="640" height="1431" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </div>
     </>
     );
