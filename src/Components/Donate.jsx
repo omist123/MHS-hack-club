522eb9406eeeefc70042dea24d548c98a4745f67
omist123 <owenmistretta@gmail.com>
2023-05-24 10:21:56 -0400

    google form

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -1,4 +1,5 @@
 function Donate() {
-  return <h1>donate</h1>;
+  return <h1 className="text-red-500 pt-20 text-3xl">donate</h1>;
 }
 export default Donate;
+   
--- src/Components/Join.jsx
+++ src/Components/Join.jsx
@@ -1,6 +1,11 @@
 function Join() {
   return (
-    <h1 className="text-red-500 pt-20 text-3xl">join</h1>
+    <>
+      <div className="flex flex-col items-center justify-center overflow-auto">
+        <h1 className="text-red-500 pt-20 text-3xl">Fill out this form to join!</h1>
+        <iframe className="pt-10 rounded-md" src="https://docs.google.com/forms/d/e/1FAIpQLScUtkN0THdFEJvHTnnC7pLSlpH7rUDlM8ojdDg5uyrREIk35Q/viewform?embedded=true" width="640" height="1248" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      </div>
+    </>
     );
 }
 export default Join;
