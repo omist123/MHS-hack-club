44515960337c25a45ed05e83ab740b606a5414f5
owen <owenmistretta@gmail.com>
2023-07-27 12:18:22 -0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,32 @@ import Home from "./Components/Home";
 import Donate from "./Components/Donate";
 import Join from "./Components/Join";
 import Contact from "./Components/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/donate":
-      component = <Donate />;
-      break;
-    case "/join":
-      component = <Join />;
-      break;
-    case "/contact":
-      component = <Contact />;
-      break;
-  }
   return (
-    <div className="App">
-      <Navbar />
-      {component}
-    </div>
+    // let component;
+    // switch (window.location.pathname) {
+    //   case "/":
+    //     component = <Home />;
+    //     break;
+    //   case "/donate":
+    //     component = <Donate />;
+    //     break;
+    //   case "/join":
+    //     component = <Join />;
+    //     break;
+    //   case "/contact":
+    //     component = <Contact />;
+    //     break;
+    <BrowserRouter>
+     <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="donate" element={<Donate />} />
+        <Route path="join" element={<Join />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
