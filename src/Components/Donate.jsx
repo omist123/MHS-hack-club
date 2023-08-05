119ff4c9695179e0cccd080cf534ee99b7435811
owen <owenmistretta@gmail.com>
2023-08-04 23:32:16 -0400

    fixe dmobile navbar

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -6,7 +6,7 @@ function Donate() {
         <br />
         Any funds received will be used for Hack Club purposes only. Examples of
         expenditures include purchasing{" "}
-        <span classNa me="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600">
           hardware
         </span>
         , software{" "}
@@ -28,7 +28,7 @@ function Donate() {
         name="donateFrame"
         scrolling="yes"
         frameBorder="0"
-        marginHeight="0px"    
+        marginHeight="0px"
         marginWidth="0px"
         allowFullScreen
         className="mr-0 "
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,38 +1,49 @@
 import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
+import { useRef, useState } from "react"; // Import useState
 import "../Styles/navbar.css";
 import logo from "../assets/hackclub-flag.svg";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const navRef = useRef();
+  const [isOpen, setIsOpen] = useState(false); // Add state to track whether the navbar is open
 
-  const showNavbar = () => {
+  const toggleNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
+    setIsOpen(!isOpen); // Toggle the isOpen state
   };
+
+  const closeNavbar = () => {
+    navRef.current.classList.remove("responsive_nav");
+    setIsOpen(false); // Ensure isOpen is set to false
+  };
+
   return (
     <div>
       <header>
         <img className="w-36 h-36 " src={logo} alt="" />
 
-        <nav ref={navRef}>
-          <Link to="/">Home</Link>
-          <Link to="/join">Join</Link>
-          <Link to="/donate">Donate</Link>
-
-          <Link className="contact-btn" to="/contact">
-            Contact
+        <nav ref={navRef} className={isOpen ? "responsive_nav" : ""}>
+          <Link to="/" onClick={closeNavbar}>
+            Home
+          </Link>
+          <Link to="/join" onClick={closeNavbar}>
+            Join
+          </Link>
+          <Link to="/donate" onClick={closeNavbar}>
+            Donate
           </Link>
 
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-            <FaTimes />
-          </button>
+          <Link className="contact-btn" to="/contact" onClick={closeNavbar}>
+            Contact
+          </Link>
         </nav>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaBars />
+        <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
+          {isOpen ? <FaTimes /> : <FaBars />}
         </button>
       </header>
     </div>
   );
 }
+
 export default Navbar;
